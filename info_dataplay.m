8a2a6078e1e600b9f50193840e3f612c863819e3
Lucy Lai <lucylai.lxl@gmail.com>
2019-04-12 16:10:31 -0400

    Update info_dataplay.m

--- info_dataplay.m
+++ info_dataplay.m
@@ -1,6 +1,11 @@
-function info_dataplay
+
 % purpose: look at harvey lab data 
-addpath('/Users/lucy/Google Drive/**Harvard**/Rotations/drugo-lab/');
+
+%- deResp: matrix of trials x neurons structure of deconvolved spike sum proxies from calcium traces
+%- mvSpd: vector of movement speeds (abstract number, larger = faster), one per trial
+%- visCon: vector of stimulus constrasts, one per trial
+%- visOri: vector of stimulus drift directions, one per trial
+addpath('/Users/lucy/Google Drive/**Harvard**/Rotations/drugo-lab/')
+
 
 load('j1_171018.mat')
-end
